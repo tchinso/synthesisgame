406590f11c867b50df5cdcf031b5720efb62cbd6
냥캣 <132228523+tchinso@users.noreply.github.com>
2026-03-19 22:14:19 +0900

    Update game.js

--- src/game.js
+++ src/game.js
@@ -21,7 +21,7 @@ const parseRecipeEntry = (entry) => {
 };
 
 const getInitialState = () => ({
-  col: 300,
+  col: 500,
   inventory: { materials: {}, items: {} },
   seen: { materials: [], items: [] },
   stories: { unlocked: ['0100'], read: [], activeId: '0100', pageIndex: 0, newIds: ['0100'] },
